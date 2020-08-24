cf971b46f4734d0f1bc0f4eddd65ca50dfebc5c2
Corey Minter <corey.minter@gmail.com>
2020-08-23 19:14:32 -0700

    fade brightness in/out

--- main.ts
+++ main.ts
@@ -1,13 +1,40 @@
+function sbBox (brightness: number) {
+    scrollbit.clear()
+    for (let row = 0; row <= rowMax; row++) {
+        scrollbit.setPixel(0, row, brightness)
+        scrollbit.setPixel(colMax, row, brightness)
+    }
+    for (let col = 0; col <= colMax; col++) {
+        scrollbit.setPixel(col, 0, brightness)
+        scrollbit.setPixel(col, rowMax, brightness)
+    }
+    scrollbit.show()
+}
+function brightUpdate () {
+    bCurrent += bChange
+    if (bCurrent > bLimits[1]) {
+        bCurrent = bLimits[1]
+        bChange = 0 - bChange
+    }
+    if (bCurrent < bLimits[0]) {
+        bCurrent = bLimits[0]
+        bChange = 0 - bChange
+    }
+}
+let bChange = 0
+let bCurrent = 0
+let bLimits: number[] = []
+let colMax = 0
+let rowMax = 0
 basic.clearScreen()
-let rowMax = scrollbit.rows() - 1
-let colMax = scrollbit.cols() - 1
+rowMax = scrollbit.rows() - 1
+colMax = scrollbit.cols() - 1
+bLimits = [0, 200]
+bCurrent = 0
+bChange = 10
 scrollbit.clear()
-for (let row = 0; row <= rowMax; row++) {
-    scrollbit.setPixel(0, row, 128)
-    scrollbit.setPixel(colMax, row, 128)
-}
-for (let col = 0; col <= colMax; col++) {
-    scrollbit.setPixel(col, 0, 128)
-    scrollbit.setPixel(col, rowMax, 128)
-}
 scrollbit.show()
+basic.forever(function () {
+    sbBox(bCurrent)
+    brightUpdate()
+})
